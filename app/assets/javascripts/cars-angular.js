3d762d22731452aae854c161f634bef5ea169f05
Albert Liao <al_liao@yahoo.com>
2014-06-24 22:17:20 -0700

    adding cost calculator

---
+++ app/assets/javascripts/cars-angular.js
@@ -0,0 +1,57 @@
+var carApp = angular.module('carApp', []);
+
+carApp.controller('OptionsListCtrl', function ($scope) {
+  $scope.total = 0.0;
+
+  $scope.options = [
+    {'category':'base','name': 'Coupe Base Price', 'price': 22000.00},
+    {'category':'fee','name': 'Destination Fee', 'price': 850.00},
+    {'category':'wheel','name': 'Silver Heli Spoke', 'price': 0.00},
+    {'category':'option','name': 'Six-Speed Automatic', 'price': 1250.00},  
+    {'category':'color','name': 'Blazing Red', 'price': 200.00}   
+  ];
+
+  $scope.init = function() {
+    $scope.updateTotal();
+  }
+
+  $scope.updateTotal = function() {
+    var total = 0;
+    for(i=0; i<$scope.options.length; i++) {
+      total = total + $scope.options[i]['price'];
+    }
+
+    $scope.total = total;  
+  }
+
+  $scope.addOption = function(option) {
+    if ($scope.options.count(function(n) { return n['name'] == option['name']; }) == 0) 
+    {
+      if (option['category'] == 'color') //clear color
+            $scope.options.remove(function(n) { return n['category'] == 'color' });
+      else if (option['category'] == 'wheel')  //clear wheel
+          $scope.options.remove(function(n) { return n['category'] == 'wheel' });
+
+      $scope.options.push(option);
+    }
+
+    $scope.updateTotal();
+
+  };
+
+  $scope.toggleEquipment = function(option) {
+    if (($scope.options.count(function(n) { return n['name'] == option['name']; }) > 0))
+      $scope.removeOption(option);
+    else
+      $scope.addOption(option);
+
+    $scope.updateTotal();
+  }
+
+  $scope.removeOption = function(option) {
+    $scope.options.remove(function(n) {
+      return n['name'] == option['name']; 
+    });
+  };
+
+});
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,19 @@
 class CarsController < ApplicationController
   def index
-  
   end
 
   def new
+  	@red = {'name' => 'Blazing Red', 'price' => 500.00, 'category' => 'color'}.to_json
+  	@white = {'name' => 'Pepper White', 'price' => 0.00, 'category' => 'color'}.to_json
+  	@blue = {'name' => 'Deep Blue', 'price' => 500.00, 'category' => 'color'}.to_json
+
+  	@wheel1 = {'category'=>'wheel','name'=>'Silver Heli Spoke', 'price'=>0.00}.to_json
+  	@wheel2 = {'category'=>'wheel','name'=>'Silver Loop Spoke', 'price'=>750.00}.to_json
+  	@wheel3 = {'category'=>'wheel','name'=>'Silver Victory Spoke', 'price'=>750.00}.to_json
+
+  	@equip1 = {'category'=>'equipment','name'=>'Six-Speed Automatic', 'price'=>1250.00}.to_json
+  	@equip2 = {'category'=>'equipment','name'=>'Chrome Line Exterior', 'price'=>150.00}.to_json
+  	@equip3 = {'category'=>'equipment','name'=>'Sports Suspension', 'price'=>300.00}.to_json
   end
 
   def summary
