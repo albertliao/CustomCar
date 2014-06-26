3c30c22b37ee8f7bea733b7a3a90ca278275263f
Albert Liao <al_liao@yahoo.com>
2014-06-25 17:03:40 -0700

    switched over to using layered images

--- app/assets/javascripts/cars-angular.js
+++ app/assets/javascripts/cars-angular.js
@@ -55,3 +55,14 @@ carApp.controller('OptionsListCtrl',['$scope', function($scope) {
   };
 
 }]);
+
+carApp.directive('backImg', function(){
+    return function(scope, element, attrs){
+        attrs.$observe('backImg', function(value) {
+            element.css({
+                'background-image': 'url(' + value +')',
+                'background-size' : 'cover'
+            });
+        });
+    };
+});
