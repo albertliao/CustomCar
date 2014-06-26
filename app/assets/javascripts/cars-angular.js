30382ee1ed4db5f28e4a5b9b68fdef56cea0e95b
Albert Liao <al_liao@yahoo.com>
2014-06-26 02:59:13 -0700

    added wheels and interiors

--- app/assets/javascripts/cars-angular.js
+++ app/assets/javascripts/cars-angular.js
@@ -3,12 +3,13 @@ var carApp = angular.module('carApp', []);
 carApp.controller('OptionsListCtrl',['$scope', function($scope) {
   $scope.total = 0.0;
   $scope.colors = [];
-  $scope.wheels = [ {'category':'wheel','name':'Silver 7-Hole','price':0.00, 'viewFront':'/assets/wheels-7hole-front.png'} ];
+  $scope.wheels = [];
+  $scope.interiors = [];
   $scope.colorSelected = 0;
   $scope.wheelSelected = 0;
+  $scope.interiorSelected = 0;
   $scope.viewPortSelected = "front";
-  // $scope.viewportBackground = "";
-  // $scope.viewportBackground = "{'background': 'url(/assets/stripe-red-front.png) 45% 30% no-repeat, url(/assets/roof-black-front.png) 45% 30% no-repeat, url(/assets/color-red-front.png) 45% 30% no-repeat, url(/assets/wheels-7hole-front.png) 45% 30% no-repeat, url(/assets/option-foglight-front.png) 45% 30% no-repeat, url(/assets/car-base-front.png) 45% 30% no-repeat, url(/assets/asphalt-front.png) 105% 45% no-repeat'}";
+  $scope.fadein = false;
 
   $scope.options = [
     {'category':'base','name': 'Coupe Base Price', 'price': 22000.00},
@@ -18,23 +19,63 @@ carApp.controller('OptionsListCtrl',['$scope', function($scope) {
     {'category':'color','name': 'Blazing Red', 'price': 200.00}   
   ];
 
-  $scope.init = function(colors) {
+  $scope.init = function(colors,wheels,interiors) {
     $scope.updateTotal();
     $scope.colors = colors;
+    $scope.wheels = wheels;
+    $scope.interiors = interiors;
   };
 
+  $scope.toFrontView = function() {
+    if ($scope.viewPortSelected == 'interior')
+      $scope.viewPortSelected = 'front';
+  }
+
   $scope.viewportBackground = function() {
-    return {
-      background: [
-                   'url('+$scope.colors[$scope.colorSelected].viewStripe+') 45% 30% no-repeat', 
-                   'url('+$scope.colors[$scope.colorSelected].viewFront+') 45% 30% no-repeat',
-                   'url('+$scope.wheels[$scope.wheelSelected].viewFront+') 45% 30% no-repeat',
-                   'url(/assets/roof-black-front.png) 45% 30% no-repeat', 
-                   'url(/assets/option-foglight-front.png) 45% 30% no-repeat',
-                   'url(/assets/car-base-front.png) 45% 30% no-repeat',
-                   'url(/assets/asphalt-front.png) 105% 45% no-repeat'
-                  ] 
-    };
+    if($scope.viewPortSelected == "front")
+      return {
+        borderColor: $scope.colors[$scope.colorSelected].borderColor,
+        background: [
+                     'url('+$scope.colors[$scope.colorSelected].viewStripeFront+') 45% 40% no-repeat', 
+                     'url('+$scope.colors[$scope.colorSelected].viewFront+') 45% 40% no-repeat',
+                     'url('+$scope.wheels[$scope.wheelSelected].viewFront+') 45% 40% no-repeat',
+                     'url(/assets/roof-black-front.png) 45% 40% no-repeat', 
+                     'url(/assets/option-foglight-front.png) 45% 40% no-repeat',
+                     'url(/assets/car-base-front.png) 45% 40% no-repeat',
+                     'url(/assets/asphalt-front.png) 105% 45% no-repeat'
+                    ],
+        transition: 'border-color 1s ease'
+      };
+    else if($scope.viewPortSelected == "rear")
+      return {
+        borderColor: $scope.colors[$scope.colorSelected].borderColor,
+        background: [
+                     'url('+$scope.colors[$scope.colorSelected].viewStripeRear+') 45% 40% no-repeat', 
+                     'url('+$scope.colors[$scope.colorSelected].viewRear+') 45% 40% no-repeat',
+                     'url('+$scope.wheels[$scope.wheelSelected].viewRear+') 45% 40% no-repeat',
+                     'url(/assets/roof-black-rear.png) 45% 40% no-repeat', 
+                     'url(/assets/car-base-rear.png) 45% 40% no-repeat',
+                     'url(/assets/asphalt-rear.png) 105% 45% no-repeat'
+                    ],
+        transition: 'border-color 1s ease' 
+      };
+    else if($scope.viewPortSelected == "interior")
+      return {
+        borderColor: $scope.colors[$scope.colorSelected].borderColor,
+        background: [
+                     'url('+$scope.interiors[$scope.interiorSelected].viewInt+') no-repeat',
+                     'url(/assets/interior-stick-shifter.png) no-repeat',
+                     'url(/assets/interior-radio.png) no-repeat',
+                     'url(/assets/interior-black-trim-top.png) no-repeat',
+                     'url(/assets/interior-black-trim.png) no-repeat',
+                     'url(/assets/interior-black-console.png) no-repeat',
+                     'url(/assets/interior-place-mats.png) no-repeat',
+                     'url(/assets/interior-mask.png) no-repeat',
+                     'url(/assets/interior-base.png) no-repeat'
+                    ],
+        transition: 'border-color 1s ease', 
+        backgroundSize: 'cover'
+      };
   };
 
   $scope.updateTotal = function() {
@@ -48,14 +89,35 @@ carApp.controller('OptionsListCtrl',['$scope', function($scope) {
 
   $scope.pickColor = function($index,option) {
     $scope.colorSelected = $index;
+
     if ($scope.options.count(function(n) { return n['name'] == option['name']; }) == 0) 
     {
       $scope.options.remove(function(n) { return n['category'] == 'color' });
       $scope.options.push(option);
     }
-
     $scope.updateTotal();
+  };
 
+  $scope.pickWheel = function($index,option) {
+    $scope.wheelSelected = $index;
+
+    if ($scope.options.count(function(n) { return n['name'] == option['name']; }) == 0) 
+    {
+      $scope.options.remove(function(n) { return n['category'] == 'wheel' });
+      $scope.options.push(option);
+    }
+    $scope.updateTotal();
+  };
+
+  $scope.pickInterior = function($index,option) {
+    $scope.interiorSelected = $index;
+
+    if ($scope.options.count(function(n) { return n['name'] == option['name']; }) == 0) 
+    {
+      $scope.options.remove(function(n) { return n['category'] == 'interior' });
+      $scope.options.push(option);
+    }
+    $scope.updateTotal();
   };
 
   $scope.toggleEquipment = function(option) {
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,22 +3,113 @@ class CarsController < ApplicationController
   end
 
   def new
-  	@red = {'name' => 'Blazing Red', 'price' => 500.00, 'category' => 'color'}.to_json
-  	@white = {'name' => 'Pepper White', 'price' => 0.00, 'category' => 'color'}.to_json
-  	@blue = {'name' => 'Deep Blue', 'price' => 500.00, 'category' => 'color'}.to_json
-
-  	@wheel1 = {'category'=>'wheel','name'=>'Silver Heli Spoke', 'price'=>0.00}.to_json
-  	@wheel2 = {'category'=>'wheel','name'=>'Silver Loop Spoke', 'price'=>750.00}.to_json
-  	@wheel3 = {'category'=>'wheel','name'=>'Silver Victory Spoke', 'price'=>750.00}.to_json
-
   	@equip1 = {'category'=>'equipment','name'=>'Six-Speed Automatic', 'price'=>1250.00}.to_json
   	@equip2 = {'category'=>'equipment','name'=>'Chrome Line Exterior', 'price'=>150.00}.to_json
   	@equip3 = {'category'=>'equipment','name'=>'Sports Suspension', 'price'=>300.00}.to_json
 
+  	@interiors = [
+  		  	{
+  			 'name' => 'Leatherette: Carbon Black', 
+  			 'desc' => 'Our sleek Carbon Black Leatherette upholstery comes with matching black door casings. (Note: Leatherette seats do not contain real leather.)',
+  			 'price' => 0.00, 
+  			 'category' => 'interior', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/interior-black-leatherette-icon.png', 
+  			 'viewInt' => '/assets/interior-black-leatherette-seat.png', 
+  			},
+  		  	{
+  			 'name' => 'Leather/Cloth: Polar Beige', 
+  			 'desc' => 'For a classic look, these Carbon Black cloth seats are contrasted by Polar Beige stitching and leather side supports, and come paired with your favorite Color Line and choice of Interior Surface options. (Note: This upholstery requires Sport Seats on Cooper models. All seating surfaces are cloth and leather; other components may be cloth or leatherette.)',
+  			 'price' => 1000.00, 
+  			 'category' => 'interior', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/interior-polar-beige-icon.png', 
+  			 'viewInt' => '/assets/interior-polar-beige-seat.png', 
+  			},
+  		  	{
+  			 'name' => 'Leather: Lounge Toffee', 
+  			 'desc' => "You won't be able to keep your hands off this Toffee Lounge leather with contrasting pipe trim. Comes paired with your choice of Color Line and Interior Surface. (Note: This upholstery requires Sport Seats on Cooper models. All seating surfaces are leather; other components may be leather or leatherette.)",
+  			 'price' => 2000.00, 
+  			 'category' => 'interior', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/interior-toffee-icon.png', 
+  			 'viewInt' => '/assets/interior-toffee-seat.png', 
+  			}
+  	]
+
+  	@wheels = [
+  			{
+  			 'name' => '15" 7-Hole', 
+  			 'desc' => '15"x 5.5" alloy wheels in our 7-Hole design. Equipped with 175/65 R15 performance tires.',
+  			 'price' => 0.00, 
+  			 'category' => 'wheel', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/wheels-7hole-icon.png', 
+  			 'viewFront' => '/assets/wheels-7hole-front.png', 
+   			 'viewRear' => '/assets/wheels-7hole-rear.png', 
+  			},
+  			{
+  			 'name' => '17" Conical Spoke', 
+  			 'desc' => '17" x 7.0" alloy wheels in our Conical Spoke design. Equipped with 205/45 R17 performance runflat tires.',
+  			 'price' => 1250.00, 
+  			 'category' => 'wheel', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/wheels-conical-silver-icon.png', 
+  			 'viewFront' => '/assets/wheels-conical-silver-front.png', 
+   			 'viewRear' => '/assets/wheels-conical-silver-rear.png', 
+  			 },
+  			{
+  			 'name' => '17" Twin Spoke Black', 
+  			 'desc' => '17" x 7.0" alloy wheels in our MINI Yours Twin Spoke design. Equipped with 205/45 R17 performance runflat tires.',
+  			 'price' => 1500.00, 
+  			 'category' => 'wheel', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/wheels-twin-black-icon.png', 
+  			 'viewFront' => '/assets/wheels-twin-black-front.png', 
+   			 'viewRear' => '/assets/wheels-twin-black-rear.png', 
+  			}
+  	]
+
   	@colors = [
-  			{'name' => 'Blazing Red', 'price' => 500.00, 'category' => 'color', 'model' => 'coupe', 'icon' => '/assets/red-color-med.png', 'viewFront' => '/assets/color-red-front.png', 'viewStripe' => '/assets/stripe-red-front.png'},
-  			{'name' => 'Pepper White', 'price' => 250.00, 'category' => 'color', 'model' => 'coupe', 'icon' => '/assets/white-color-med.png', 'viewFront' => '/assets/color-white-front.png', 'viewStripe' => '/assets/stripe-white-front.png'},
-  			{'name' => 'Deep Blue', 'price' => 300.00, 'category' => 'color', 'model' => 'coupe', 'icon' => '/assets/blue-color-med.png', 'viewFront' => '/assets/color-blue-front.png', 'viewStripe' => '/assets/stripe-blue-front.png'}
+  			{
+  			 'name' => 'Chili Red', 
+  			 'price' => 0.00, 
+  			 'desc' => "NON-METALLIC PAINT - Fiery and feisty, Chili Red is eager to blaze a trail. Just make sure the gas pedal doesn't get too hot to handle, as MINIs in this bold color have a way of attracting a little extra attention - from admirers and Pamplona bulls alike.",
+  			 'category' => 'color', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/red-color-med.png', 
+  			 'viewFront' => '/assets/color-red-front.png', 
+  			 'viewStripeFront' => '/assets/stripe-red-front.png',
+   			 'viewRear' => '/assets/color-red-rear.png', 
+  			 'viewStripeRear' => '/assets/stripe-red-rear.png',
+  			 'borderColor' => 'red'
+  			},
+  			{
+  			 'name' => 'Pepper White', 
+  			 'desc' => "NON-METALLIC PAINT - Pepper White's timeless, understated style is underscored by an effortless gentility and freewheeling spirit. When paired with a black roof and bonnet stripes, this color wields all the power of a well-cut tux, with none of the dry cleaning bills. Your tailor would approve.",
+  			 'price' => 0.00, 
+  			 'category' => 'color', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/white-color-med.png', 
+  			 'viewFront' => '/assets/color-white-front.png', 
+  			 'viewStripeFront' => '/assets/stripe-white-front.png',
+   			 'viewRear' => '/assets/color-white-rear.png', 
+  			 'viewStripeRear' => '/assets/stripe-white-rear.png',
+  			 'borderColor' => 'white'
+  			 },
+  			{
+  			 'name' => 'Lightning Blue',
+  			 'desc' => "METALLIC PAINT - Some claim it was Zeus' preferred color for the palace atop Mount Olympus, while others argue it was color of the horn from which Thor drank the ocean. One thing's for certain - Lightning Blue is a color worthy of its name.", 
+  			 'price' => 500.00, 
+  			 'category' => 'color', 
+  			 'model' => 'coupe', 
+  			 'icon' => '/assets/blue-color-med.png', 
+  			 'viewFront' => '/assets/color-blue-front.png', 
+  			 'viewStripeFront' => '/assets/stripe-blue-front.png',
+   			 'viewRear' => '/assets/color-blue-rear.png', 
+  			 'viewStripeRear' => '/assets/stripe-blue-rear.png',
+  			 'borderColor' => 'blue'
+  			}
   	]
   end
 
